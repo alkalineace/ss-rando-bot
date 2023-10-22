f20825d65a042053e6fc28c8a4542f23915e50ba
alkalineace <alkalineace@gmail.com>
2023-10-22 03:24:27 +0200

    presets and versions

--- randobot/website.py
+++ randobot/website.py
@@ -4,15 +4,64 @@ import requests
 
 class Website:
     preset_endpoint = 'https://ssrando.com/api/dynamicdata/racePresets'
+    github_endpoint = 'https://api.github.com/repos/ssrando/ssrando/releases'
+    sha_endpoint = 'https://api.github.com/repos/ssrando/ssrando/git/ref/tags/'
 
     def __init__(self):
-        self.presets = self.load_presets()
+        self.load_presets()
+        self.versions = self.load_versions()
 
     def load_presets(self):
         presets = requests.get(self.preset_endpoint).json()
 
-        preset_list = {}
-        for array in presets:
-            preset_list[array["data"]["settings"]] = array["data"]["name"]
+        bit_preset_list = {}
+        nobit_preset_list = {}
+        for array_preset in presets:
+            if array_preset['data'].get('bit') is None:
+                nobit_preset_list[array_preset["data"]["settings"]] = array_preset["data"]["name"]
+            else:
+                bit_preset_list[array_preset["data"]["settings"]] = array_preset["data"]["name"]
 
-        return preset_list
+        self.bit_presets = bit_preset_list
+        self.nobit_presets = nobit_preset_list
+    
+    def load_versions(self):
+        params = {'per_page': 5, 'page': 1}
+        versions = requests.get(self.github_endpoint, params=params).json()
+
+        if versions.get('tag_name') is None:
+            return {"2.1.1_f389925": "Latest (2.1.1_f389925)"}
+
+        tags = []
+        for array_version in versions:
+            tags.append(array_version['tag_name'])
+
+        version_list = {}
+        for tag in tags:
+            hashes = requests.get(self.sha_endpoint + tag).json()
+
+            if tag[0] != 'v':
+                tag = "v" + tag
+
+            if hashes.get('object') is None:
+                return {"2.1.1_f389925": "Latest (2.1.1_f389925)"}
+            
+            if hashes['object'].get('sha') is None:
+                return {"2.1.1_f389925": "Latest (2.1.1_f389925)"}
+
+            version = tag[1:] + "_" + hashes['object']['sha'][:7]
+
+            if version_list != {}:
+                version_list[version] = version
+            else:
+                version_list[version] = "Latest (" + version + ")"
+
+        return version_list
+    
+    def reload_presets(self):
+        print("trying to reload presets")
+        temp = self.load_presets()
+
+        if temp is not {}:
+            print("successfully recovered presets")
+            self.presets = temp
